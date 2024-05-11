714cffdad5bbd41e616007f062552bbce990800d
amansen09.github.io <amansen774@gmail.com>
2024-05-11 11:14:17 +0530

    Nodeserver
    Nodeserver

---
+++ Nodeserver/index.js
@@ -0,0 +1,33 @@
+//  node server which will handle socket io connection
+const io = require('socket.io')(8000)
+
+// const cors =require('cors');
+// users.use(cors());
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        // console.log("New user", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+    socket.on('send', message => {
+        socket.broadcast.emit('recieve', { message: message, name: users[socket.id] })
+    });
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+
+    });
+
+
+});
+
+
+
+
+
+
+
+
